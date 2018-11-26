ffd7517a58fe75e723bc971c87f4e8d1cb457052
Steven Eardley <steve@eardley.xyz>
2018-11-26 14:15:11 +0000

    initial movement from rotation

--- arm.py
+++ arm.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python
 import sys
 import time
-import pygame
 import RPi.GPIO as GPIO
+import pygame
 
 # Use BCM GPIO references instead of physical pin numbers
 GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
 
 # Define GPIO pins to use for each of the motors.
-ROTPINS = [4,17,21,22]
-SH1PINS = [10,9,11,7]
-SH2PINS = [23,24,25,8]
+ROTPINS = (4,17,21,22)
+SH1PINS = (10,9,11,7)
+SH2PINS = (23,24,25,8)
 
 # Half step sequence
 STEPSEQ = [[1,0,0,1],
@@ -25,58 +26,72 @@ STEPSEQ = [[1,0,0,1],
 OFF = [0,0,0,0]
 
 class ArmStepperMotor(object):
-    pins = []
+    pins = None
     clockwise = True
-    step_delay = 0.01 #seconds
     next_step_index = 0
-    seq = []
-    
-    def __init__(pins, delay=10, clockwise=True, **kwargs):
+    seq = None
+
+    def __init__(self, pins, clockwise=True, **kwargs):
         self.pins = pins
         for p in self.pins:
-            GPIO.setup(pin,GPIO.OUT)
-            GPIO.output(pin, False)
-        self.stepDelay = delay / 1000.00
+            GPIO.setup(p, GPIO.OUT)
+            GPIO.output(p, False)
+        global STEPSEQ
         if clockwise:
-            seq = [zip(self.pins, s) for s in STEPSEQ]
+            self.seq = [list(zip(self.pins, s)) for s in STEPSEQ]
         else:
-            seq = [zip(self.pins, s) for s in reversed(STEPSEQ)]
-        
-    def step(forwards=True):
+            self.seq = [list(zip(self.pins, s)) for s in reversed(STEPSEQ)]
+
+    def step(self, forwards=True):
         # Apply the next step instruction to the motor controller
-        for inst_set in seq[self.next_step_index]:
-            [GPIO.output(pin, bool(sig)) for (pin, sig) in inst_set]
-            time.sleep(self.stepDelay)
+        [GPIO.output(pin, bool(sig)) for (pin, sig) in self.seq[self.next_step_index]]
         if forwards:
             self.next_step_index += 1
         else:
             self.next_step_index -=1
-        
+
         # Set the index to our next step in the sequence
         if self.next_step_index >= len(self.seq):
             self.next_step_index = 0
         elif self.next_step_index < 0:
-            self.next_step_index = len(self.seq - 1)
-            
-    def stop()
+            self.next_step_index = len(self.seq) - 1
+
+    def stop(self):
         [GPIO.output(pin, bool(sig)) for (pin, sig) in zip(self.pins, OFF)]
-            
 
+m1 = ArmStepperMotor(ROTPINS, clockwise=False)
+m2 = ArmStepperMotor(SH1PINS)
+m3 = ArmStepperMotor(SH2PINS)
 
-m1 = ArmStepperMotor(pins=ROTPINS)
-m2 = ArmStepperMotor(pins=SH1PINS)
-m3 = ArmStepperMotor(pins=SH2PINS)
-
-
-pygame.key.init()
+m1_on = False
+pygame.init()
+clock = pygame.time.Clock()
 while True:
+    m1.step(forwards=False)
+    clock.tick(100)
+    """
     keys = pygame.key.get_pressed()
-    print(keys)
     if keys[pygame.K_LEFT]:
+        print("left")
         m1.step()
     if keys[pygame.K_RIGHT]:
+        print("right")
         m1.step(forwards=False)
     if keys[pygame.K_ESCAPE]:
         m1.stop()
         exit(0)
-    
+    for event in pygame.event.get():
+        if m1_on:
+            m1.step()
+        else:
+            m1.stop()
+        if event.type == pygame.QUIT:
+            m1.stop()
+            exit(0)
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                m1_on = True
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT:
+                m1_on = False
+    """
