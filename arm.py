fe4736f663a05e1195027ffa5fea103fc2a5f1f3
Steven Eardley <steve@eardley.xyz>
2018-11-26 12:52:05 +0000

    added basic control for rotation

--- arm.py
+++ arm.py
@@ -1,71 +1,82 @@
 #!/usr/bin/python
 import sys
 import time
+import pygame
 import RPi.GPIO as GPIO
 
-# Use BCM GPIO references
-# instead of physical pin numbers
+# Use BCM GPIO references instead of physical pin numbers
 GPIO.setmode(GPIO.BCM)
 
-# Define GPIO signals to use
-# Physical pins 11,15,16,18
-# GPIO17,GPIO22,GPIO23,GPIO24
-Step1Pins = [4,17,21,22]
-Step2Pins = [10,9,11,7]
-Step3Pins = [23,24,25,8]
+# Define GPIO pins to use for each of the motors.
+ROTPINS = [4,17,21,22]
+SH1PINS = [10,9,11,7]
+SH2PINS = [23,24,25,8]
 
-# Set all pins as output
-for pin in Step1Pins + Step2Pins + Step3Pins:
-  print "Setup pins"
-  GPIO.setup(pin,GPIO.OUT)
-  GPIO.output(pin, False)
+# Half step sequence
+STEPSEQ = [[1,0,0,1],
+           [1,0,0,0],
+           [1,1,0,0],
+           [0,1,0,0],
+           [0,1,1,0],
+           [0,0,1,0],
+           [0,0,1,1],
+           [0,0,0,1]]
 
-# Define advanced sequence
-# as shown in manufacturers datasheet
-Seq = [[1,0,0,1],
-       [1,0,0,0],
-       [1,1,0,0],
-       [0,1,0,0],
-       [0,1,1,0],
-       [0,0,1,0],
-       [0,0,1,1],
-       [0,0,0,1]]
+OFF = [0,0,0,0]
 
-StepCount = len(Seq)
-StepDir = -1 # Set to 1 or 2 for clockwise
-            # Set to -1 or -2 for anti-clockwise
+class ArmStepperMotor(object):
+    pins = []
+    clockwise = True
+    step_delay = 0.01 #seconds
+    next_step_index = 0
+    seq = []
+    
+    def __init__(pins, delay=10, clockwise=True, **kwargs):
+        self.pins = pins
+        for p in self.pins:
+            GPIO.setup(pin,GPIO.OUT)
+            GPIO.output(pin, False)
+        self.stepDelay = delay / 1000.00
+        if clockwise:
+            seq = [zip(self.pins, s) for s in STEPSEQ]
+        else:
+            seq = [zip(self.pins, s) for s in reversed(STEPSEQ)]
+        
+    def step(forwards=True):
+        # Apply the next step instruction to the motor controller
+        for inst_set in seq[self.next_step_index]:
+            [GPIO.output(pin, bool(sig)) for (pin, sig) in inst_set]
+            time.sleep(self.stepDelay)
+        if forwards:
+            self.next_step_index += 1
+        else:
+            self.next_step_index -=1
+        
+        # Set the index to our next step in the sequence
+        if self.next_step_index >= len(self.seq):
+            self.next_step_index = 0
+        elif self.next_step_index < 0:
+            self.next_step_index = len(self.seq - 1)
+            
+    def stop()
+        [GPIO.output(pin, bool(sig)) for (pin, sig) in zip(self.pins, OFF)]
+            
 
-# Read wait time from command line
-if len(sys.argv)>1:
-  WaitTime = int(sys.argv[1])/float(1000)
-else:
-  WaitTime = 10/float(1000)
 
-# Initialise variables
-StepCounter = 0
+m1 = ArmStepperMotor(pins=ROTPINS)
+m2 = ArmStepperMotor(pins=SH1PINS)
+m3 = ArmStepperMotor(pins=SH2PINS)
 
-# Start main loop
+
+pygame.key.init()
 while True:
-
-  print StepCounter,
-  print Seq[StepCounter]
-
-  for pin in range(0, 4):
-    xpin = Step1Pins[pin]
-    if Seq[StepCounter][pin]!=0:
-      print " Enable GPIO %i" %(xpin)
-      GPIO.output(xpin, True)
-    else:
-      GPIO.output(xpin, False)
-
-  StepCounter += StepDir
-
-  # If we reach the end of the sequence
-  # start again
-  if (StepCounter>=StepCount):
-    StepCounter = 0
-  if (StepCounter<0):
-    StepCounter = StepCount+StepDir
-
-  # Wait before moving on
-  time.sleep(WaitTime)
+    keys = pygame.key.get_pressed()
+    print(keys)
+    if keys[pygame.K_LEFT]:
+        m1.step()
+    if keys[pygame.K_RIGHT]:
+        m1.step(forwards=False)
+    if keys[pygame.K_ESCAPE]:
+        m1.stop()
+        exit(0)
+    
